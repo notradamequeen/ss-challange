be922231318b8cb0070047d58776b7cbb5e41670
febby <febby.oktaviani@docdoc.com>
2020-10-26 00:32:43 +0700

    WEB-BE: remove cache and add gitignore

--- db/db/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE sourcesagedb;
-CREATE USER 'sourcesageuser'@'localhost' IDENTIFIED BY 'password!';
-GRANT ALL PRIVILEGES ON sourcesagedb.* TO 'sourcesageuser'@'locahost' IDENTIFIED BY 'password!';
